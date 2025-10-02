ccf98c9ba0459e8fea97ad4f956f84d16eaae28a
1Kr3ksu <bielawskowski@gmail.com>
2025-10-02 12:25:11 +0200

    etap 3 skonczony , teraz etap 4 do zrobienia

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -3,7 +3,7 @@ import './CoffeeCard.css'
 function CoffeeCard({ name, type, description, newPosition }) {
     return (
         <div className="coffee-card">
-            <h3>{name} {newPosition && <span className="new-badge">NOWOŚĆ</span>}</h3>
+            <h3>{name} {newPosition && <span className="new-position">NOWOŚĆ</span>}</h3>
             <h4 className="coffee-type">{type}</h4>
             <p className="coffee-description">{description}</p>
         </div>
